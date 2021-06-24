59cc2c82efbd07339405d718eeb700b5f37737a9
PG-India <yogeshBinary>
2021-06-24 19:09:23 +0530

    Testing

---
+++ src/test/java/com/yogi/location/location/LocationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yogi.location.location;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
